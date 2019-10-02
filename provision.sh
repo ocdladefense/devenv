726bbd330b9361df941389174c17f5657d80a17c
Jose Bernal <jbernal.web.dev@gmail.com>
2019-10-02 20:56:30 +0000

    Initial shell provisioner.

--- provision.sh
+++ provision.sh
@@ -3,10 +3,11 @@
 # Create an XML file with the Apache2 <VirtualHost> directives.
 # php create-virtual-host "wc19" "wc19.ocdla.org" > ${SITE_NAME}.conf
 # 
-
+WP_INSTALL_URL = "https://wordpress.org/latest.tar.gz"
 # Create an Apache2 <VirtualHost> directive
 sudo cp ~/${site-name}.conf /etc/apache2/site-available/{$SITE_NAME}.conf
-
+wget WP_INSTALL_URL ~/wp.tar.gz
+tar -xvzf ~/wp.tar.gz > /var/www/{$SITE_NAME}/wordpress
 
 # Create the DOCUMENT_ROOT
 mkdir /var/www/{$SITE_NAME}
@@ -18,6 +19,11 @@ chmod g+w /var/www/{$SITE_NAME}
 
 # ACL on directory
 setfacl -d -m g::rwx /var/www/{$SITE_NAME}
+
+
+
+# Extract WordPress files to DOCUMENT_ROOT
+tar -xvzf latest.tar.gz -C /var/www/wc19
 
 
 
